82b6180eb04812205eed6f07e92a5dbd0647f48e
YerinJun <erin2jj@gmail.com>
2021-06-29 20:49:12 +0900

    numbering src

---
+++ java_programming/src3/Variable01.java
@@ -0,0 +1,13 @@
+import java.sql.SQLOutput;
+
+public class Variable01 {
+    public static void main(String[] args){
+        int number = 3;
+        System.out.println(number);
+        System.out.println(number);
+        System.out.println(number);
+        number=10;
+        System.out.println(number);
+        System.out.println(number);
+    }
+}
---
+++ java_programming/src3/Variable06.java
@@ -0,0 +1,17 @@
+public class Variable06 {
+    public static void main(String[] args){
+        int number1 = 3;
+        int number2 = number1;
+        System.out.println(number2);
+        System.out.println(number1);
+        System.out.println(number1+number2);
+        number1 = 5;
+        System.out.println(number1);
+        System.out.println(number2);
+        System.out.println(number1+number2);
+        // int number2 = number1 으로 정의 되어 있는데 왜 마지막 계산식에서 10이 아닌 8값이 나오는지 모르겠다.ㅠㅠ
+        /* 공부하다 보면 배우겠지
+           그렇지만 지금 당장 궁금하다 ㅜㅜㅜㅜㅜㅜㅜㅜㅜ
+         */
+    }
+}
